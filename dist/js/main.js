dfd8add8f9b9a6ec0939e9de6e000f9f4769aefe
talia <zemla.talia@gmail.com>
2019-09-23 14:14:49 -0500

    thanos for cells works

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,9 +1,16 @@
 $(function () {
   const gameBoard = document.querySelector(".game__board");
+  console.log(gameBoard);
   const thanos = document.querySelector(".game__thanos__img");
   const test = document.querySelector("#cell-1");
 
   thanosEffect(thanos, gameBoard);
   thanosEffect(test, test);
+
+  const allCells = document.querySelectorAll('.game__board__item');
+
+  for (let cell of allCells) {
+    thanosEffect(cell, cell);
+  }
 
 });
